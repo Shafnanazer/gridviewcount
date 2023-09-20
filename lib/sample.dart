b61589fe7080c7bd0ab3047bdd89114c23fb7cc9
Shafnanazer <98227310+Shafnanazer@users.noreply.github.com>
2023-09-21 01:53:45 +0530

    Initial commit

---
+++ lib/sample.dart
@@ -0,0 +1,28 @@
+import'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: sample(),));
+}
+
+class sample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        appBar: AppBar(title: Text("My App"),),
+        body: Container(color:Colors.cyan,
+          child: Center(
+            child: Text("Welcome to flutter", style: TextStyle(fontSize: 30,
+                color: Colors.red[900],
+                fontStyle: FontStyle.italic,
+                fontWeight:FontWeight.w600,),
+                ),
+          ),
+        )
+
+
+    );
+  }
+
+
+}
